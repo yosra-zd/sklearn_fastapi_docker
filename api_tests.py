c2f6ba270c21f245a8ccf6d13e96f3d02e9b9ea1
Ubuntu <ubuntu@ip-172-31-25-119.eu-west-1.compute.internal>
2022-06-23 11:35:13 +0000

    add api tests

--- api_tests.py
+++ api_tests.py
@@ -1,6 +1,6 @@
 import requests
 
-API_URL = "http://34.245.151.68:8000"
+API_URL = "http://127.0.0.1:8000"
 
 def test_model_info():
     response = requests.get(
@@ -20,11 +20,13 @@ def test_service_health():
     data = response.json()
 
     assert data["health"] == "ok"
-	
-def test_model_predict():
-    response = requests.post(
-        url=f"{API_URL}/predict",
-		params={'tenure': 29,
+from app.main import api
+from fastapi.testclient import TestClient	
+def test1_model_predict():	
+    client = TestClient(api)
+
+    Input={
+           'tenure': 29,
             'paperless_billing': 1,
             'internet_service_fiber_optic': 0,
             'no_internet_service': 1,
@@ -32,6 +34,24 @@ def test_model_predict():
             'device_protection': 0,
             'contract_month_to_month': 0,
             'payment_method_electronic_check': 1}
+    response = client.post('/predict', json=input)
+    assert response.status_code == 200, response.content
+    data = response.json()
+
+    assert data["prediction"] == 0	
+Input={
+           'tenure': 29,
+            'paperless_billing': 1,
+            'internet_service_fiber_optic': 0,
+            'no_internet_service': 1,
+            'online_security': 0,
+            'device_protection': 0,
+            'contract_month_to_month': 0,
+            'payment_method_electronic_check': 1}
+def test_model_predict():
+    response = requests.post(
+        url=f"{API_URL}/predict",
+	params="{'input':{}}".format(Input)
         # url="{}/predict".format(API_URL)
     )
 
