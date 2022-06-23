91791240ddf915d2c27bb1690a39ce3f43737141
yosra-zd <101110818+yosra-zd@users.noreply.github.com>
2022-06-23 13:24:06 +0100

    Create api_tests.py

---
+++ api_tests.py
@@ -0,0 +1,42 @@
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+
+def test_model_info():
+    response = requests.get(
+        url=f"{API_URL}/"
+        # url="{}/".format(API_URL)
+    )
+
+    assert response.status_code == 200, response.content
+
+def test_service_health():
+    response = requests.get(
+        url=f"{API_URL}/health"
+        # url="{}/health".format(API_URL)
+    )
+
+    assert response.status_code == 200, response.content
+    data = response.json()
+
+    assert data["health"] == "ok"
+#import main
+from .main import api
+from fastapi.testclient import TestClient
+def test1_model_predict():
+    client = TestClient(api)
+
+    Input={
+           'tenure': 29,
+            'paperless_billing': 1,
+            'internet_service_fiber_optic': 0,
+            'no_internet_service': 1,
+            'online_security': 0,
+            'device_protection': 0,
+            'contract_month_to_month': 0,
+            'payment_method_electronic_check': 1}
+    response = client.post('/predict', json=input)
+    assert response.status_code == 200, response.content
+    data = response.json()
+
+    assert data["prediction"] == 0
