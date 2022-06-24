d4400d2d2f3491824195285d9c2c638a0341e9c0
yosra-zd <101110818+yosra-zd@users.noreply.github.com>
2022-06-24 15:00:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ async def batch_predict(file: UploadFile = File(...)):
     #    raise HTTPException(status_code=400, detail="File format provided is not valid.")
     #content = await file.read()
     data_churn=prepare_data(file)
-    return data_churn[0]
+    return data_churn.to_dict()
     #response = batch_file_predict(data_churn)
      # return the response as a JSON
     #return {
