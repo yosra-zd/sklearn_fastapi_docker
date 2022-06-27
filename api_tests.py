21cf5b47d65ddb6ef7e007bbcb37286a2b35fd7a
yosra-zd <101110818+yosra-zd@users.noreply.github.com>
2022-06-27 13:13:00 +0100

    Update api_tests.py

--- api_tests.py
+++ api_tests.py
@@ -41,8 +41,7 @@ def test_model_predict():
     data = response.json()
 
     assert data["prediction"] == 0
-    
-    def test_batch_predict():	
+ def test_batch_predict():	
 
         client = TestClient(api)
 
