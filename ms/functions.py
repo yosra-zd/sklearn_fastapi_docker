b4829a787a257cea63a1ebf409b7687969fcafa2
yosra-zd <101110818+yosra-zd@users.noreply.github.com>
2022-06-28 17:54:13 +0100

    Update functions.py

--- ms/functions.py
+++ ms/functions.py
@@ -36,32 +36,32 @@ def get_model_response(input):
 	
 def prepare_data(df):
     #if file is not None:
-       # importing the dataset
-       #file="data/batch_churn.csv"
-       #df = pd.read_csv(file,sep=',')
-       #df = pd.read_csv(file)
-       # drop de customerID qui n'est pas une information utile
-       df.drop(columns='customerID', inplace=True)
-       # Transformation des labels en données binaires
-       df.gender = pd.Series(np.where(df.gender.values == 'Male', 1, 0), df.index)
-       df.Partner = pd.Series(np.where(df.Partner.values == 'Yes', 1, 0), df.index)
-       df.Dependents = pd.Series(np.where(df.Dependents.values == 'Yes', 1, 0), df.index)
-       df.PhoneService = pd.Series(np.where(df.PhoneService.values == 'Yes', 1, 0), df.index)
-       df.SeniorCitizen = pd.Series(np.where(df.SeniorCitizen.values == 'Yes', 1, 0), df.index)
-       df.PaperlessBilling = pd.Series(np.where(df.PaperlessBilling.values == 'Yes', 1, 0), df.index)
-       # Correction du type de données de TotalChurn
-       # on commence par remplacer les champs vide de la colonne TotalCharges par 0 
-       df.TotalCharges = pd.Series(np.where(df.tenure == 0, 0, df.TotalCharges), df.index)
-       # On change ensuite le type de la colonne TotalCharges en type numérique float 
-       # df.TotalCharges = pd.to_numeric(df.TotalCharges, errors='coerce')
-       df.TotalCharges = pd.to_numeric(df.TotalCharges, downcast="float")
-	   #feature scaling
-       sc = MinMaxScaler()
-       df['tenure'] = sc.fit_transform(df[['tenure']])
-       df['MonthlyCharges'] = sc.fit_transform(df[['MonthlyCharges']])
-       df['TotalCharges'] = sc.fit_transform(df[['TotalCharges']])
-       df = df[['tenure','PaperlessBilling','InternetService','OnlineSecurity','DeviceProtection','Contract','PaymentMethod']]
-       columns=['tenure', 
+    # importing the dataset
+    #file="data/batch_churn.csv"
+    #df = pd.read_csv(file,sep=',')
+    #df = pd.read_csv(file)
+    # drop de customerID qui n'est pas une information utile
+    df.drop(columns='customerID', inplace=True)
+    # Transformation des labels en données binaires
+    df.gender = pd.Series(np.where(df.gender.values == 'Male', 1, 0), df.index)
+    df.Partner = pd.Series(np.where(df.Partner.values == 'Yes', 1, 0), df.index)
+    df.Dependents = pd.Series(np.where(df.Dependents.values == 'Yes', 1, 0), df.index)
+    df.PhoneService = pd.Series(np.where(df.PhoneService.values == 'Yes', 1, 0), df.index)
+    df.SeniorCitizen = pd.Series(np.where(df.SeniorCitizen.values == 'Yes', 1, 0), df.index)
+    df.PaperlessBilling = pd.Series(np.where(df.PaperlessBilling.values == 'Yes', 1, 0), df.index)
+    # Correction du type de données de TotalChurn
+    # on commence par remplacer les champs vide de la colonne TotalCharges par 0 
+    df.TotalCharges = pd.Series(np.where(df.tenure == 0, 0, df.TotalCharges), df.index)
+    # On change ensuite le type de la colonne TotalCharges en type numérique float 
+    # df.TotalCharges = pd.to_numeric(df.TotalCharges, errors='coerce')
+    df.TotalCharges = pd.to_numeric(df.TotalCharges, downcast="float")
+    #feature scaling
+    sc = MinMaxScaler()
+    df['tenure'] = sc.fit_transform(df[['tenure']])
+    df['MonthlyCharges'] = sc.fit_transform(df[['MonthlyCharges']])
+    df['TotalCharges'] = sc.fit_transform(df[['TotalCharges']])
+    df = df[['tenure','PaperlessBilling','InternetService','OnlineSecurity','DeviceProtection','Contract','PaymentMethod']]
+    columns=['tenure', 
    	       'PaperlessBilling',
    	       'InternetService_Fiber optic',
    	       'InternetService_No',
@@ -69,10 +69,9 @@ def prepare_data(df):
   	       'DeviceProtection_Yes',
    	       'Contract_Month-to-month',
     	       'PaymentMethod_Electronic check']
-	df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
-        #Encoding the other categorical categoric features with more than two categories
-	
-        return df
+    df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
+    #Encoding the other categorical categoric features with more than two categories
+    return df
 def batch_file_predict(input):    
     model=load_model()
     #Get batch prediction
