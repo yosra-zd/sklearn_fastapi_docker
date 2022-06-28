3a0de09cdef140a3c55280afaba58285a5f676a2
yosra-zd <101110818+yosra-zd@users.noreply.github.com>
2022-06-28 16:04:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,8 @@ async def batch_predict(file: UploadFile = File(...)):
     buffer.close()
     data_churn = prepare_data(df)
     response = batch_file_predict(data_churn)
-    response.to_csv('data/result.csv',sep='\t')
+    prediction_result='data/result.csv'+file.filename
+    response.to_csv(prediction_result,sep='\t')
     #return response.to_json()
     return {
         "filename": file.filename,
