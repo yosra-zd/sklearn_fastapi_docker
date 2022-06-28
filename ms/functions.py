bc45e0197ff9441723b1ba98dffdc27c01b1fed2
yosra-zd <101110818+yosra-zd@users.noreply.github.com>
2022-06-28 17:29:36 +0100

    Update functions.py

--- ms/functions.py
+++ ms/functions.py
@@ -73,7 +73,7 @@ def prepare_data(df):
     	       'PaymentMethod_Electronic check']
         #Encoding the other categorical categoric features with more than two categories
         df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
-       return df
+        return df
 	
 def batch_file_predict(input):    
     model=load_model()
